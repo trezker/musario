3d1ca5efabbf136316299f301f590f9b40330f59
Anders Andersson <anders@linux-9d93.site>
2009-06-21 20:27:07 +0200

    Player exploring rooms

--- test/start.lua
+++ test/start.lua
@@ -2,23 +2,38 @@ print("Welcome to the test game")
 
 table.insert(commands, "open door")
 
-open_door = function()
+open_door = function(dirstring)
 	if not data.rooms then
 		print("You open the door and take your first step into the unknown, unless you played this game before.")
 		data.rooms = {}
 		data.playerx = 1
 		data.playery = 1
+	elseif dirstring == "n" then
+		data.playery = data.playery+1
+	elseif dirstring == "s" then
+		data.playery = data.playery-1
+	elseif dirstring == "e" then
+		data.playerx = data.playerx+1
+	elseif dirstring == "w" then
+		data.playerx = data.playerx-1
+	else
+		print("Usage: open door [n,s,e,w]")
 	end
+
 
 	if not data.rooms[data.playerx] then
 		data.rooms[data.playerx] = {}
 	end
-	data.rooms[data.playerx][data.playery] = {}
+	if not data.rooms[data.playerx][data.playery] then
+		print("You explore a new room")
+		data.rooms[data.playerx][data.playery] = {}
+		room = data.rooms[data.playerx][data.playery]
+	end
 end
 
 explore_cmd_handler = function(cmd)
 	if cmd:sub(1,9) == "open door" then
-		open_door()
+		open_door(cmd:sub(11))
 	end
 end
 
